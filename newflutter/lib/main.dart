4f277055a52492b1c6f970b3466dc1a6a8dfba94
jeffyea320 <128056568+jeffyea320@users.noreply.github.com>
2023-04-04 14:49:25 +0900

    [frontend] 메세지 아이콘 추가

--- newflutter/lib/main.dart
+++ newflutter/lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Icon(Icons.phone),
+                Icon(Icons.message),
               ],
             )
           ),
