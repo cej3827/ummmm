331adf7bbe7d6ca61d5bfe1e5ce062660ec27ca4
KwonHaeJin <kwonhaejin1781@gmail.com>
2023-05-15 20:07:12 +0900

    [front]해진

--- newflutter/lib/main.dart
+++ newflutter/lib/main.dart
@@ -1,74 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:newflutter/widget/bottom_bar.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatefulWidget{
+class MyApp extends StatefulWidget {
   MyApp({Key? key}) : super(key: key);
+
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  late TabController controller;
   @override
   build(context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 4,
-          child: Scaffold(
-            appBar: AppBar(
-              title: Text("앱이름",style: TextStyle(color: Colors.purple),),
-              centerTitle: true,
-              backgroundColor: Colors.white,
-              leading: IconButton(
-                  onPressed: (){
-                    Navigator.pop(context); //뒤로가기
-                  },
-                  color: Colors.purple,
-                  icon : Icon(Icons.arrow_back)
-              ),
-            ),
-
-            body: TabBarView(
-              children: [
-                Container(
-                  child: Text("오늘의 질문"),
-                ),
-                Container(
-                  child: Text("질문리스트"),
-                ),
-                Container(
-                  child: Text("캘린더"),
-                ),
-                Container(
-                  child: Text("MY"),
-                ),
-              ],
-            ),
-            extendBodyBehindAppBar: true,
-            bottomNavigationBar: BottomBar(),
-          )
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "앱이름",
+          style: TextStyle(color: Colors.purple),
         ),
-        body: Container(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Center(child: Text("오늘의 질문"),),
-              Center(child: Text("Q.오늘 뭐 먹었어?", style: TextStyle(color: Colors.purple),)),
-              Center(
-                child: ElevatedButton(
-                    child: Text("답변하기"),
-                    onPressed: (){},
-                ),
-              )
-            ],
-          ),
-        ),// bottomNavigationBar: BAB(),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context); //뒤로가기
+            },
+            color: Colors.purple,
+            icon: Icon(Icons.arrow_back)),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Text("오늘의 질문"),
+            ),
+            Center(
+                child: Text(
+              "Q.오늘 뭐 먹었어?",
+              style: TextStyle(color: Colors.purple),
+            )),
+            Center(
+              child: ElevatedButton(
+                child: Text("답변하기"),
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
+      ),
+      // bottomNavigationBar: BAB(),
     );
   }
 }
