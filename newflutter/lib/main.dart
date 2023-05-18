e41e1d454803962243ff661feadae298edf172f7
jeffyea320 <128056568+jeffyea320@users.noreply.github.com>
2023-05-18 21:31:14 +0900

    [front] 질문리스트창 만들었는데 Card안됨 슬픔... 답변하기 팝업도 만들었우

--- newflutter/lib/main.dart
+++ newflutter/lib/main.dart
@@ -31,7 +31,7 @@ class _MyAppState extends State<MyApp> {
           icon: Icon(Icons.arrow_back),
         ),
       ),
-      body: [question(), Text('list'), Text('calender'), Text('My')][tab],
+      body: [question(), qlist(), Text('calender'), Text('My')][tab],
 
       bottomNavigationBar: BottomNavigationBar(
         showUnselectedLabels: false,
@@ -41,7 +41,6 @@ class _MyAppState extends State<MyApp> {
             tab = i;
           });
         },
-
         items: [
           BottomNavigationBarItem(
             label: '오늘의 질문',
@@ -112,6 +111,8 @@ class bottombar extends StatelessWidget {
   } // build
 } // bottombar
 */
+
+//오늘의 질문 창
 class question extends StatelessWidget {
   const question({Key? key}) : super(key: key);
 
@@ -120,76 +121,138 @@ class question extends StatelessWidget {
     return Center(
       child: Container(
         padding: EdgeInsets.fromLTRB(10, 50, 10, 50), // 상하좌우 여백주기
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(
-              child: Text(
-                "오늘의 질문",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            SizedBox(
-              // 사이 여백 주기
-              height: 90,
-            ),
-            SizedBox(
-              child: Text(
-                "Q. 오늘 뭐 먹었어?",
-                style: TextStyle(fontSize: 40, fontWeight: FontWeight.normal),
-              ),
-            ),
-            SizedBox(
-              // 사이 여백 주기
-              height: 30,
-            ),
-            SizedBox(
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFD05DFF),
-                  fixedSize: Size(130, 50),
-                ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
                 child: Text(
-                  "답변하기",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  "오늘의 질문",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-                onPressed: () {}, // 버튼 누르는 효과
               ),
-            ),
-            SizedBox(
-              // 사이 여백 주기
-              height: 90,
-            ),
-            SizedBox(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(
-                    Icons.account_circle,
-                    size: 40,
-                    color: Colors.cyan,
-                  ),
-                  Icon(
-                    Icons.account_circle,
-                    size: 40,
-                    color: Colors.red,
-                  ),
-                  Icon(Icons.account_circle, size: 40, color: Colors.blue),
-                  Icon(Icons.account_circle, size: 40, color: Colors.amber),
-                  Icon(
-                    Icons.account_circle,
-                    size: 40,
-                    color: Colors.brown,
-                  ),
-                  Icon(Icons.account_circle, size: 40, color: Colors.green),
-                ],
+              SizedBox(
+                // 사이 여백 주기
+                height: 90,
               ),
-            ),
-          ], // children
+              SizedBox(
+                child: Text(
+                  "Q. 오늘 뭐 먹었어?",
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.normal),
+                ),
+              ),
+              SizedBox(
+                // 사이 여백 주기
+                height: 30,
+              ),
+              SizedBox(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFD05DFF),
+                    fixedSize: Size(130, 50),
+                  ),
+                  child: Text(
+                    "답변하기",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext con) {
+                          return AlertDialog(
+                            title: Text("오늘 뭐 먹었어?"),
+                            content: SingleChildScrollView(
+                              child: TextField(
+                                keyboardType: TextInputType.multiline,
+                                maxLines: null,
+                                decoration: InputDecoration(
+                                    contentPadding: EdgeInsets.all(40)),
+                              ),
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: Text("제출"),
+                              ),
+                              TextButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: Text("취소"),
+                              ),
+                            ],
+                          );
+                        });
+                  }, // 버튼 누르는 효과
+                ),
+              ),
+              SizedBox(
+                // 사이 여백 주기
+                height: 90,
+              ),
+              SizedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(
+                      Icons.account_circle,
+                      size: 40,
+                      color: Colors.cyan,
+                    ),
+                    Icon(
+                      Icons.account_circle,
+                      size: 40,
+                      color: Colors.red,
+                    ),
+                    Icon(Icons.account_circle, size: 40, color: Colors.blue),
+                    Icon(Icons.account_circle, size: 40, color: Colors.amber),
+                    Icon(
+                      Icons.account_circle,
+                      size: 40,
+                      color: Colors.brown,
+                    ),
+                    Icon(Icons.account_circle, size: 40, color: Colors.green),
+                  ],
+                ),
+              ),
+            ], // children
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// 질문 리스트 창
+
+class qlist extends StatelessWidget {
+  const qlist({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.fromLTRB(10, 50, 10, 50), // 상하좌우 여백주기
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                child: ListView.builder(
+                  itemCount: 3,
+                  itemBuilder: (context, i) {
+                    return const Card(
+                      child: ListTile(
+                        title: Text("제발 돌아가라고오오오ㅗ"),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
