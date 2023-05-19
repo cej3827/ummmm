955e51c4ac7fb88a8462afd314ae5ee9ac33367f
jeffyea320 <128056568+jeffyea320@users.noreply.github.com>
2023-05-19 20:47:55 +0900

    [front] 파일분리 앤나 질문리스트페이지 만들기 앤나 답장 만들다 말음

---
+++ newflutter/lib/question.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+//오늘의 질문 창
+class question extends StatelessWidget {
+  const question({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.fromLTRB(10, 50, 10, 50), // 상하좌우 여백주기
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                child: Text(
+                  "오늘의 질문",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                // 사이 여백 주기
+                height: 90,
+              ),
+              SizedBox(
+                child: Text(
+                  "Q. 오늘 뭐 먹었어?",
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.normal),
+                ),
+              ),
+              SizedBox(
+                // 사이 여백 주기
+                height: 30,
+              ),
+              SizedBox(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFD05DFF),
+                    fixedSize: Size(130, 50),
+                  ),
+                  child: Text(
+                    "답변하기",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext con) {
+                          return AlertDialog(
+                            title: Text("오늘 뭐 먹었어?"),
+                            content: SingleChildScrollView(
+                              child: TextField(
+                                keyboardType: TextInputType.multiline,
+                                maxLines: null,
+                                decoration: InputDecoration(
+                                    contentPadding: EdgeInsets.all(40)),
+                              ),
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: Text("제출"),
+                              ),
+                              TextButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: Text("취소"),
+                              ),
+                            ],
+                          );
+                        });
+                  }, // 버튼 누르는 효과
+                ),
+              ),
+              SizedBox(
+                // 사이 여백 주기
+                height: 90,
+              ),
+              SizedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(
+                      Icons.account_circle,
+                      size: 40,
+                      color: Colors.cyan,
+                    ),
+                    Icon(
+                      Icons.account_circle,
+                      size: 40,
+                      color: Colors.red,
+                    ),
+                    Icon(Icons.account_circle, size: 40, color: Colors.blue),
+                    Icon(Icons.account_circle, size: 40, color: Colors.amber),
+                    Icon(
+                      Icons.account_circle,
+                      size: 40,
+                      color: Colors.brown,
+                    ),
+                    Icon(Icons.account_circle, size: 40, color: Colors.green),
+                  ],
+                ),
+              ),
+            ], // children
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ newflutter/lib/questionlist.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+//DB에 저장된거 받아와야하는데 어케하는지 몰라서 일단 배열..
+final questions = ['Q1. 오늘 뭐 먹었어?', 'Q2. 잠온다', 'Q3. 집가고싶다', 'Q4.', 'Q5.', 'Q6.', 'Q7.', 'Q8.', 'Q9.'];
+final answers = ['1','2','3','4','5','6','7','8','9',];
+
+class qlist extends StatelessWidget {
+  const qlist({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ListView.builder(
+      itemCount: questions.length,
+      itemBuilder: (context, index) {
+        return Card( //                           <-- Card widget
+          child: ListTile(
+            title: Text(questions[index]),
+            onTap: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (c){return Text(questions[index]);
+                    }
+                ),
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
+
+//질문리스트에서 질문 누르면 답변 나오는 새창 만드는 중..
+/*
+class answerpopup0 extends StatelessWidget {
+  const answerpopup0({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(questions[index]),
+
+        ],
+      ),
+    );
+  }
+}
+*/
