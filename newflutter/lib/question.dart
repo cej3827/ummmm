180ae59aa86812577e978d01f0aa0e48875fe342
jeffyea320 <128056568+jeffyea320@users.noreply.github.com>
2023-07-21 16:18:14 +0900

    [front] 디자인 수정

--- newflutter/lib/question.dart
+++ newflutter/lib/question.dart
@@ -1,87 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:timer_builder/timer_builder.dart';
+import 'package:intl/intl.dart';
+var now = new DateTime.now();
 
 //오늘의 질문 창
 class question extends StatelessWidget {
-  const question({Key? key}) : super(key: key);
+  question({Key? key}) : super(key: key);
+
+  String formatDate = DateFormat('yy /MM /dd').format(now);
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        padding: EdgeInsets.fromLTRB(30, 30, 30, 30), // 상하좌우 여백주기
+        padding: EdgeInsets.fromLTRB(0, 30, 0, 30), // 상하좌우 여백주기
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 30,), //여백
-              SizedBox(
-                child: Text(
-                  "오늘의 질문",
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color : Color(
-                        0xFF788648)
-                  ),
-                ),
-              ),
-              SizedBox(height: 30,), //여백
-              SizedBox(
-                child: Text(
-                  " : 오늘 뭐 먹었어?",
-                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.normal, color : Color(
-                      0xFF788648)),
-                ),
-              ),
-              SizedBox(
-                // 사이 여백 주기
-                height: 30,
-              ),
-              SizedBox(
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Color(0xFFFFA84E),
-                    fixedSize: Size(400, 40),
-                  ),
-                  child: Text(
-                    "답변하기",
-                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color : Colors.white),
-                  ),
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext con) {
-                          return AlertDialog(
-                            title: Text("오늘 뭐 먹었어?"),
-                            content: SingleChildScrollView(
-                              child: TextField(
-                                keyboardType: TextInputType.multiline,
-                                maxLines: null,
-                                decoration: InputDecoration(
-                                    contentPadding: EdgeInsets.all(40)),
-                              ),
-                            ),
-                            actions: [
-                              TextButton(
-                                onPressed: () => Navigator.of(context).pop(),
-                                child: Text("제출"),
-                              ),
-                              TextButton(
-                                onPressed: () => Navigator.of(context).pop(),
-                                child: Text("취소"),
-                              ),
-                            ],
-                          );
-                        });
-                  }, // 버튼 누르는 효과
-                ),
-              ),
-              SizedBox(
-                // 사이 여백 주기
-                height: 70,
-              ),
-              SizedBox(
+              SizedBox(height: 10,),
+              Container(
+                height: 120,
+                padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                color: Color(0x83CBCCCA),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
@@ -124,6 +66,71 @@ class question extends StatelessWidget {
                   ],
                 ),
               ),
+              SizedBox(height: 10,),//여백
+              Container(
+                padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                child: SnackBar(
+                  content: Text("$formatDate"),
+                ),
+              ),
+              SizedBox(height: 10,), //여백
+              Container(
+                padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                child: Text(
+                  " : 오늘 뭐 먹었어?",
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.normal, color : Color(
+                      0xFF788648)),
+                ),
+              ),
+              SizedBox(
+                // 사이 여백 주기
+                height: 30,
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFFFA84E),
+                    fixedSize: Size(400, 40),
+                  ),
+                  child: Text(
+                    "답변하기",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, color : Colors.white),
+                  ),
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext con) {
+                          return AlertDialog(
+                            title: Text("오늘 뭐 먹었어?"),
+                            content: SingleChildScrollView(
+                              child: TextField(
+                                keyboardType: TextInputType.multiline,
+                                maxLines: null,
+                                decoration: InputDecoration(
+                                    contentPadding: EdgeInsets.all(40)),
+                              ),
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: Text("제출"),
+                              ),
+                              TextButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: Text("취소"),
+                              ),
+                            ],
+                          );
+                        });
+                  }, // 버튼 누르는 효과
+                ),
+              ),
+              SizedBox(
+                // 사이 여백 주기
+                height: 10,
+              ),
+
             ], // children
           ),
         ),
