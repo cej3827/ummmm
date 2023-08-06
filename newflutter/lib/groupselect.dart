f9ddb9c7f6c20e11d1cab8a76b8dc6476977432a
jeffyea320 <128056568+jeffyea320@users.noreply.github.com>
2023-08-06 17:38:33 +0900

    [front] 로그인창 그룹선택창 생성 앤나 연결

---
+++ newflutter/lib/groupselect.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:newflutter/main.dart';
+
+class groupSelect extends StatefulWidget {
+  const groupSelect({Key? key}) : super(key: key);
+
+  @override
+  State<groupSelect> createState() => _groupSelectState();
+}
+
+class _groupSelectState extends State<groupSelect> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF788648),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              width: 320,
+              height: 250,
+              color: Colors.white,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(height: 30,),
+                  Text("접속할 그룹을 선택하세요.",style: TextStyle(fontSize: 20,color: Colors.black),),
+                  SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 130, height: 120,
+                        child: IconButton(
+                            onPressed: (){
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context)=>MyApp())
+                              );
+                            },
+                            icon: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Icon(Icons.account_circle,size: 80),
+                                Text("멧돌과 맹구",style: TextStyle(fontSize: 17),),
+                              ],
+                            )
+                        ),
+                      ),
+                      SizedBox(width: 10,),
+                      SizedBox(
+                        width: 130, height: 120,
+                        child: IconButton(
+                            onPressed: (){},
+                            icon: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Icon(Icons.add_box_outlined,size: 80,),
+                                Text("그룹 추가",style: TextStyle(fontSize: 17),)
+                              ],
+                            )
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- newflutter/lib/login.dart
+++ newflutter/lib/login.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:newflutter/groupselect.dart';
 import 'package:newflutter/main.dart';
 import 'package:newflutter/question.dart';
 import 'style.dart' as style;
 import 'package:intl/date_symbol_data_local.dart';
 
-void main() async{
+void main() async {
   await initializeDateFormatting();
   runApp(MaterialApp(theme: style.theme, home: MyLogin()));
 }
@@ -19,16 +20,113 @@ class MyLogin extends StatefulWidget {
 class _MyLoginState extends State<MyLogin> {
   var tab = 0;
 
+  final loginId = TextEditingController();
+  final loginPw = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF788648),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (_) => MyApp()));
-          },
-          child: Text("Go to Main Page"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 0.5,
+                    blurRadius: 0.5,
+                    offset: Offset(1, 1),
+                  )
+                ],
+              ),
+              width: 350,
+              height: 450,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Container(
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Container(
+                          width: 7,
+                          height: 60,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SizedBox(
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 50),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
+                    child: Column(
+                      children: [
+                        TextField(
+                          controller: loginId,
+                          decoration: InputDecoration(hintText: '아이디'),
+                        ),
+                        TextField(
+                          controller: loginPw,
+                          decoration: InputDecoration(hintText: '비민번호'),
+                        ),
+                        TextButton(
+                            onPressed: (){},
+                            child: Text('아이디/비밀번호 찾기',style: TextStyle(fontSize: 13,color: Color(0xFFFFA84E)),)
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    child: Column(
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context)=>const groupSelect())
+                              );
+                            },
+                            child: Text('로그인',style: TextStyle(fontSize:17,color: Colors.white,fontWeight: FontWeight.bold),),
+                            style: ElevatedButton.styleFrom(backgroundColor: Color(
+                                0xFF727272),fixedSize: Size(200, 30))
+                        ),
+                        ElevatedButton(
+                            onPressed: () {},
+                            child: Text('새 계정 만들기',style: TextStyle(fontSize:17,color: Colors.black,fontWeight: FontWeight.bold),),
+                            style: ElevatedButton.styleFrom(backgroundColor: Color(
+                                0xFFB6B6B6),fixedSize: Size(200, 30))
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
