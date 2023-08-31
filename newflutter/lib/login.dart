772cec941ad619887d481ded57a3afef36d230fb
KwonHaeJin <103015007+KwonHaeJin@users.noreply.github.com>
2023-08-31 21:48:11 +0900

    [front] 로그인안됨,,,

--- newflutter/lib/login.dart
+++ newflutter/lib/login.dart
@@ -17,21 +17,30 @@ final loginPw = TextEditingController();
 String loginId ='';
 String loginPw = '';
 
-Future<String> postLoginRequest(String user_ID, String user_PW) async{
-  String userID = loginId;
-  String userPW = loginPw;
+class User {
+  final String user_ID;
+  final String user_PW;
+  //final Map<String, dynamic> company;
 
-  var response = await http.post(
-    Uri.parse('http://43.202.31.89:8080/user/login'),
-    headers: <String, String>{
-      'Content-Type' : 'application/json',
-    },
-    body: jsonEncode({
-      "user_ID": userID,
-      "user_PW": userPW
-    }),
-  );
-  return response.body;
+  User({required this.user_ID,required this.user_PW,/*required this.company*/});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      user_ID: json['user_ID'],
+      user_PW: json['user_PW'],
+      //company: userMap['company'],
+    );
+  }
+}
+
+Future<User> fetchUser() async{
+  final response = await http.get(Uri.https('http://43.202.31.89:8080/user/login'));
+
+  if(response.statusCode == 200){
+    return User.fromJson(json.decode(response.body));
+  }
+  else{
+  throw Exception('데이터 수신 실패');}
 }
 
 void main() async {
@@ -49,6 +58,13 @@ class MyLogin extends StatefulWidget {
 class _MyLoginState extends State<MyLogin> {
   var tab = 0;
 
+  late Future<User> user;
+
+  @override
+  void initState(){
+    super.initState();
+    user = fetchUser();
+  }
   //final loginId = TextEditingController();
   //final loginPw = TextEditingController();
 
@@ -57,117 +73,134 @@ class _MyLoginState extends State<MyLogin> {
     return Scaffold(
       backgroundColor: Color(0xFF788648),
       body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 0.5,
-                      blurRadius: 0.5,
-                      offset: Offset(1, 1),
-                    )
-                  ],
-                ),
-                width: 350,
-                height: 450,
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 60,
+        child: FutureBuilder<User>(
+          future: user,
+          builder: (context, snapshot) {
+            return SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 0.5,
+                          blurRadius: 0.5,
+                          offset: Offset(1, 1),
+                        )
+                      ],
                     ),
-                    Container(
-                      child: Row(
-                        children: [
-                          SizedBox(
-                            width: 20,
+                    width: 350,
+                    height: 450,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 60,
+                        ),
+                        Container(
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Container(
+                                width: 7,
+                                height: 60,
+                                color: Colors.grey,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              SizedBox(
+                                child: Text(
+                                  'Login',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold, fontSize: 50),
+                                ),
+                              )
+                            ],
                           ),
-                          Container(
-                            width: 7,
-                            height: 60,
-                            color: Colors.grey,
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Container(
+                          padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                decoration: InputDecoration(hintText: '아이디'),
+                              ),
+                              TextFormField(
+                                decoration: InputDecoration(hintText: '비밀번호'),
+                              ),
+                              TextButton(
+                                  onPressed: (){
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(builder: (context)=>const idFindPage())
+                                    );
+                                  },
+                                  child: (Text('아이디/비밀번호 찾기',style: TextStyle(fontSize: 13,color: Color(0xFFFFA84E)),))
+                              )
+                            ],
                           ),
-                          SizedBox(
-                            width: 10,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              ElevatedButton(
+                                  onPressed: (){
+                                    if(snapshot.hasData){
+                                      print(snapshot.data!.user_ID);
+                                      print(snapshot.data!.user_PW);
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(builder: (context)=>const groupSelect())
+                                      );}
+                                    else if(snapshot.hasError){
+                                      print('에러');
+                                      print(snapshot.data!.user_ID);
+                                      print(snapshot.data!.user_PW);
+                                    };
+                                  },
+                                  child: Text('로그인',style: TextStyle(fontSize:17,color: Colors.white,fontWeight: FontWeight.bold),),
+                                  style: ElevatedButton.styleFrom(backgroundColor: Color(
+                                      0xFF727272),fixedSize: Size(200, 30))
+                              ),
+                              ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(builder: (context)=>const join())
+                                    );
+                                  },
+                                  child: Text('새 계정 만들기',style: TextStyle(fontSize:17,color: Colors.black,fontWeight: FontWeight.bold),),
+                                  style: ElevatedButton.styleFrom(backgroundColor: Color(
+                                      0xFFB6B6B6),fixedSize: Size(200, 30))
+                              ),
+                            ],
                           ),
-                          SizedBox(
-                            child: Text(
-                              'Login',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 50),
-                            ),
-                          )
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
-                      child: Column(
-                        children: [
-                          TextFormField(
-                            decoration: InputDecoration(hintText: '아이디'),
-                          ),
-                          TextFormField(
-                            decoration: InputDecoration(hintText: '비밀번호'),
-                          ),
-                          TextButton(
-                              onPressed: (){
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(builder: (context)=>const idFindPage())
-                                );
-                              },
-                              child: (Text('아이디/비밀번호 찾기',style: TextStyle(fontSize: 13,color: Color(0xFFFFA84E)),))
-                          )
-                        ],
-                      ),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      child: Column(
-                        children: [
-                          ElevatedButton(
-                              onPressed: (){
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(builder: (context)=>const groupSelect())
-                                  );
-                              },
-                              child: Text('로그인',style: TextStyle(fontSize:17,color: Colors.white,fontWeight: FontWeight.bold),),
-                              style: ElevatedButton.styleFrom(backgroundColor: Color(
-                                  0xFF727272),fixedSize: Size(200, 30))
-                          ),
-                          ElevatedButton(
-                              onPressed: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(builder: (context)=>const join())
-                                );
-                              },
-                              child: Text('새 계정 만들기',style: TextStyle(fontSize:17,color: Colors.black,fontWeight: FontWeight.bold),),
-                              style: ElevatedButton.styleFrom(backgroundColor: Color(
-                                  0xFFB6B6B6),fixedSize: Size(200, 30))
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            );
+          }
         ),
-      ),
+    ),
     );
   } // build
+}
+
+Widget buildColumn(snapshot) {
+  return Column();
 }
